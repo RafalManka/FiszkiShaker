89cf957e63c1e11c6a32b0ccc2025ee37628e159
Rafał Mańka <rafal@RafalManka-Ubuntu.(none)>
2013-07-06 19:28:47 +0200

    redesigned, animating strings.

--- src/pl/rafalmanka/harrypotter/spellshaker/MainActivity.java
+++ src/pl/rafalmanka/harrypotter/spellshaker/MainActivity.java
@@ -3,7 +3,6 @@ package pl.rafalmanka.harrypotter.spellshaker;
 import pl.rafalmanka.fiszki.shaker.R;
 import pl.rafalmanka.harrypotter.spellshaker.ShakeDetector.OnShakeListener;
 import android.app.Activity;
-import android.graphics.drawable.AnimationDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
@@ -13,40 +12,49 @@ import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.animation.AlphaAnimation;
-import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private final String TAG = "MainActivity";
 	private TextView mWordOrig;
 	private TextView mWordTranslation;
-	private ImageView mFiszkiImage;
 	private SensorManager mSensorManager;
 	private Sensor mAccelerometer;
 	private ShakeDetector mShakeDetector;
 	private boolean mTitleActive = false;
 	private Word mWord;
 	private String mLanguage = "pl";
+	private Animation bounce;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "OnCreated");
 		super.onCreate(savedInstanceState);
 
+		Log.d(TAG, "creating layout of an activity");
 		setContentView(R.layout.activity_main);
-		Log.d(TAG, "OnCreated");
-		mWordOrig = (TextView) findViewById(R.id.textView1);
-		mWordTranslation = (TextView) findViewById(R.id.TextView11);
-		mFiszkiImage = (ImageView) findViewById(R.id.imageView1);
+
+		Log.d(TAG, "creating instance of bouncing animation");
+		bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+		Log.d(TAG, "assigning strings from layout to variables");
+		mWordOrig = (TextView) findViewById(R.id.bouncing_string);
+		mWordTranslation = (TextView) findViewById(R.id.bouncing_description);
+
+		Log.d(TAG, "creating instance of sensor event");
 		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		mAccelerometer = mSensorManager
 				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		Log.d(TAG, "creating onshakelistener");
 		mShakeDetector = new ShakeDetector(new OnShakeListener() {
 			@Override
 			public void onShake() {
-				handleNewAnswer();
+				Log.d(TAG, "onSheked");
+				onEvent();
 			}
 		});
-
 	}
 
 	@Override
@@ -80,52 +88,49 @@ public class MainActivity extends Activity {
 		});
 	}
 
-	private void animateBackgroundImage() {
-		mFiszkiImage.setImageResource(R.drawable.magician_animation);
-		AnimationDrawable fiszkiAnimation = (AnimationDrawable) mFiszkiImage
-				.getDrawable();
-		if (fiszkiAnimation.isRunning()) {
-			fiszkiAnimation.stop();
-		}
-		fiszkiAnimation.start();
-
-	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
-	private void handleNewAnswer() {
-		Log.d(TAG, "onClicked");
-		if (mTitleActive) {
-			// if title is visible show description
-			mWordTranslation.setText(Html.fromHtml(mWord.getDescription()));
-			Log.d(TAG, "showing description: " + mWord.getDescription());
-			animateFiszka(mWordTranslation, 1500);
-			Log.d(TAG, "animating description");
-			mTitleActive = false;
-		} else {
-			// if title and description is visible
-			// generate new pair of strings and show title
+	private void onEvent() {
+		Log.d(TAG, "onEvented");
+		if (!mTitleActive) {
 			Log.d(TAG, "preparing database");
 			DatabaseHandler db = new DatabaseHandler(this);
+
 			Log.d(TAG, "fetching single random row");
 			mWord = db.getRandom(mLanguage);
-			String log = "Id: " + mWord.getID() + " ,word: " + mWord.getWord()
+			Log.d(TAG, "Id: " + mWord.getID() + " ,word: " + mWord.getWord()
 					+ " ,description: " + Html.fromHtml(mWord.getDescription())
-					+ " , language: " + mWord.getLanguage();
-			// Writing Contacts to log
-			Log.d(TAG, log);
+					+ " , language: " + mWord.getLanguage());
 
-			mWordTranslation.setText("");
+			Log.d(TAG,
+					"deleting description text, setting word text to new value");
 			mWordOrig.setText(Html.fromHtml(mWord.getWord()));
-			animateFiszka(mWordOrig, 1500);
-			mTitleActive = true;
-		}
+			mWordTranslation.setText("");
 
-		animateBackgroundImage();
+			Log.d(TAG, "animating title");
+			mWordOrig.startAnimation(bounce);
+			// animateFiszka(mWordOrig, 1500);
+
+			Log.d(TAG, "setting flag, that the title is showing");
+			mTitleActive = true;
+
+		} else {
+
+			Log.d(TAG, "showing description: " + mWord.getDescription());
+			mWordTranslation.setText(Html.fromHtml(mWord.getDescription()));
+
+			Log.d(TAG, "animating description");
+			animateFiszka(mWordTranslation, 1500);
+
+			Log.d(TAG, "setting flag, that the title is NOT showing");
+			mTitleActive = false;
+
+			Log.d(TAG, "animating background phase 2");
+		}
 
 		playSound();
 	}
